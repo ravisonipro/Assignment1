7c6139931f83c66258a41bfb2522980980eb9348
ravi soni <ravi.soni45452@gmail.com>
2023-12-08 23:25:49 +0530

    minor changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       flash[:notice]="Signup successful"
+      redirect_to blogs_path
     else
       flash[:notice]="Please try again"
       redirect_to '/signup'
